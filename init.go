9c8bc3ff2c1e47b483f99d827522cafed8af2f3f
vista <vistova@gmail.com>
2017-01-10 17:14:35 +0300

    Some attributes for RFC 2869

--- init.go
+++ init.go
@@ -66,4 +66,9 @@ func init() {
 	Builtin.MustRegister("Acct-Terminate-Cause", 49, AttributeInteger)
 	Builtin.MustRegister("Acct-Multi-Session-Id", 50, AttributeText)
 	Builtin.MustRegister("Acct-Link-Count", 51, AttributeInteger)
+
+	//RFC 2869 (not full, see https://www.ietf.org/rfc/rfc2869.txt)
+	Builtin.MustRegister("Acct-Input-Gigawords", 52, AttributeInteger)
+	Builtin.MustRegister("Acct-Output-Gigawords", 53, AttributeInteger)
+	Builtin.MustRegister("NAS-Port-ID", 87, AttributeString)
 }
