5ab2e4b632770d21565c514b6defe4c5ca6aa68e
Tim Cooper <tim.cooper@layeh.com>
2017-07-30 15:58:22 -0300

    add RFC 2867

--- generate.go
+++ generate.go
@@ -1,4 +1,5 @@
 //go:generate go run cmd/radius-dict-gen/main.go -package rfc2865 -output rfc2865/generated.go /usr/share/freeradius/dictionary.rfc2865
 //go:generate go run cmd/radius-dict-gen/main.go -package rfc2866 -output rfc2866/generated.go /usr/share/freeradius/dictionary.rfc2866
+//go:generate go run cmd/radius-dict-gen/main.go -package rfc2867 -output rfc2867/generated.go -ref Acct-Status-Type:layeh.com/radius/rfc2866 /usr/share/freeradius/dictionary.rfc2867
 
 package radius
