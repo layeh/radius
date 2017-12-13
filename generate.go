71d04d52724abcd9915a3dc8d6c8941cf1252335
Tim Cooper <tim.cooper@layeh.com>
2017-12-13 19:47:24 -0400

    add rfc2869 (partial)
    fixes #8

--- generate.go
+++ generate.go
@@ -1,6 +1,7 @@
 //go:generate go run cmd/radius-dict-gen/main.go -package rfc2865 -output rfc2865/generated.go /usr/share/freeradius/dictionary.rfc2865
 //go:generate go run cmd/radius-dict-gen/main.go -package rfc2866 -output rfc2866/generated.go /usr/share/freeradius/dictionary.rfc2866
 //go:generate go run cmd/radius-dict-gen/main.go -package rfc2867 -output rfc2867/generated.go -ref Acct-Status-Type:layeh.com/radius/rfc2866 /usr/share/freeradius/dictionary.rfc2867
+//go:generate go run cmd/radius-dict-gen/main.go -package rfc2869 -ignore Event-Timestamp -ignore ARAP-Password -ignore ARAP-Features -ignore EAP-Message -ignore ARAP-Challenge-Response -output rfc2869/generated.go /usr/share/freeradius/dictionary.rfc2869
 //go:generate go run cmd/radius-dict-gen/main.go -package rfc3576 -output rfc3576/generated.go -ref Service-Type:layeh.com/radius/rfc2865 /usr/share/freeradius/dictionary.rfc3576
 //go:generate go run cmd/radius-dict-gen/main.go -package rfc5176 -output rfc5176/generated.go -ref Error-Cause:layeh.com/radius/rfc3576 /usr/share/freeradius/dictionary.rfc5176
 
