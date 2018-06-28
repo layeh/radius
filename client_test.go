e73505903aee686768ca8af4357fbfca9d7f64b7
Tim Cooper <tim.cooper@layeh.com>
2018-06-28 17:37:34 -0300

    add tests for Client.Exchange invalid, non-authentic packets

--- client_test.go
+++ client_test.go
@@ -3,6 +3,7 @@ package radius
 import (
 	"context"
 	"net"
+	"strings"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -90,5 +91,58 @@ func TestClient_Exchange_cancelled(t *testing.T) {
 	}
 	if err != context.Canceled {
 		t.Fatalf("got error = %v; expecting context.Canceled", err)
+	}
+}
+
+func TestClient_Exchange_invalidPacket(t *testing.T) {
+	secret := []byte(`12345`)
+
+	var server *TestServer
+	handler := HandlerFunc(func(w ResponseWriter, r *Request) {
+		// write bad data to client
+		server.l.WriteTo([]byte(`AAAA`), r.RemoteAddr)
+	})
+	server = newTestServer(handler, StaticSecretSource(secret))
+	defer server.Close()
+
+	req := New(CodeAccessRequest, secret)
+
+	client := Client{
+		Retry:           time.Millisecond * 5,
+		MaxPacketErrors: 2,
+	}
+	resp, err := client.Exchange(context.Background(), req, server.Addr)
+	if resp != nil {
+		t.Fatalf("got non-nil response (%v); expected nil", resp)
+	}
+	if expecting := `packet not at least 20 bytes long`; !strings.Contains(err.Error(), expecting) {
+		t.Fatalf("got error = %v; expecting %s", err, expecting)
+	}
+}
+
+func TestClient_Exchange_nonauthenticPacket(t *testing.T) {
+	secret := []byte(`12345`)
+
+	var server *TestServer
+	handler := HandlerFunc(func(w ResponseWriter, r *Request) {
+		resp := r.Response(CodeAccessAccept)
+		resp.Authenticator = [16]byte{}
+		w.Write(resp)
+	})
+	server = newTestServer(handler, StaticSecretSource(secret))
+	defer server.Close()
+
+	req := New(CodeAccessRequest, secret)
+
+	client := Client{
+		Retry:           time.Millisecond * 5,
+		MaxPacketErrors: 2,
+	}
+	resp, err := client.Exchange(context.Background(), req, server.Addr)
+	if resp != nil {
+		t.Fatalf("got non-nil response (%v); expected nil", resp)
+	}
+	if _, ok := err.(*NonAuthenticResponseError); !ok {
+		t.Fatalf("got error %T; expecting NonAuthenticResponseError", err)
 	}
 }
