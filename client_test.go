0f5078ae951fb9080eec04874936f8ad06a3915c
Tim Cooper <tim.cooper@layeh.com>
2018-06-28 17:09:15 -0300

    add test for Client.Retry

--- client_test.go
+++ client_test.go
@@ -3,6 +3,7 @@ package radius
 import (
 	"context"
 	"net"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -31,5 +32,35 @@ func TestClient_Exchange_expired(t *testing.T) {
 		t.Fatal("err is not a net.Error")
 	} else if !netErr.Timeout() {
 		t.Fatal("got netErr.Timeout() = false; expected true")
+	}
+}
+
+func TestClient_Exchange_retry(t *testing.T) {
+	secret := []byte(`12345`)
+
+	var attempts int32
+
+	handler := HandlerFunc(func(w ResponseWriter, r *Request) {
+		if atomic.AddInt32(&attempts, 1) == 4 {
+			w.Write(r.Response(CodeAccessAccept))
+		}
+	})
+	server := newTestServer(handler, StaticSecretSource(secret))
+	defer server.Close()
+
+	req := New(CodeAccessRequest, secret)
+
+	client := Client{
+		Retry: time.Millisecond * 5,
+	}
+	resp, err := client.Exchange(context.Background(), req, server.Addr)
+	if err != nil {
+		t.Fatalf("got err %s; expected nil", err)
+	}
+	if resp.Code != CodeAccessAccept {
+		t.Fatalf("got code %s; expected %s", resp.Code, CodeAccessAccept)
+	}
+	if attempts := atomic.LoadInt32(&attempts); attempts != 4 {
+		t.Fatalf("response received in %d attemps; expecting 4", attempts)
 	}
 }
