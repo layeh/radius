53323aa0076a15e3e85ab6f251db4cb83e7060b5
Tim Cooper <tim.cooper@layeh.com>
2018-10-12 20:56:41 -0500

    change dictionarygen tests to just examine exported symbols

--- dictionarygen/generator_test.go
+++ dictionarygen/generator_test.go
@@ -2,6 +2,12 @@ package dictionarygen
 
 import (
 	"bytes"
+	"fmt"
+	"go/ast"
+	"go/doc"
+	"go/parser"
+	"go/printer"
+	"go/token"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -32,15 +38,15 @@ func TestTestData(t *testing.T) {
 
 	for _, tt := range tbl {
 		t.Run(tt.Name, func(t *testing.T) {
-			parser := &dictionary.Parser{
+			dictParser := &dictionary.Parser{
 				Opener: &dictionary.FileSystemOpener{},
 			}
 			if tt.InitParser != nil {
-				tt.InitParser(parser)
+				tt.InitParser(dictParser)
 			}
 
 			dictFile := filepath.Join("testdata", tt.Name+".dictionary")
-			dict, err := parser.ParseFile(dictFile)
+			dict, err := dictParser.ParseFile(dictFile)
 			if err != nil {
 				t.Fatalf("could not parse file: %s", err)
 			}
@@ -63,22 +69,71 @@ func TestTestData(t *testing.T) {
 				t.Fatalf("could not generate dictionary code: %s", err)
 			}
 
+			docs, err := generateGoDoc(generatedCode)
+			if err != nil {
+				t.Fatalf("could not generate docs: %s", err)
+			}
+
 			generatedFile := filepath.Join("testdata", tt.Name+".generated")
-			if err := ioutil.WriteFile(generatedFile, generatedCode, 0644); err != nil {
+			if err := ioutil.WriteFile(generatedFile, docs, 0644); err != nil {
 				t.Fatalf("could not write generated file: %s", err)
 			}
 
 			expectedFile := filepath.Join("testdata", tt.Name+".expected")
-			expectedCode, err := ioutil.ReadFile(expectedFile)
+			expectedDocs, err := ioutil.ReadFile(expectedFile)
 			if err != nil {
 				t.Fatalf("could not read expected output: %s", err)
 			}
 
-			if !bytes.Equal(generatedCode, expectedCode) {
-				t.Fatal("generated code does not equal expected")
+			if !bytes.Equal(docs, expectedDocs) {
+				t.Fatal("generated docs do not equal expected")
 			}
 
 			os.Remove(generatedFile)
 		})
 	}
+}
+
+func generateGoDoc(source []byte) ([]byte, error) {
+	fs := token.NewFileSet()
+
+	f, err := parser.ParseFile(fs, "source.go", source, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	pkg := &ast.Package{
+		Name: "test",
+		Files: map[string]*ast.File{
+			"source.go": f,
+		},
+	}
+
+	d := doc.New(pkg, "", 0)
+
+	var b bytes.Buffer
+	printCfg := printer.Config{
+		Indent:   1,
+		Tabwidth: 8,
+		Mode:     printer.TabIndent,
+	}
+
+	fmt.Fprintf(&b, "Constants:\n")
+	for _, c := range d.Consts {
+		if err := printCfg.Fprint(&b, fs, c.Decl); err != nil {
+			return nil, err
+		}
+		fmt.Fprintf(&b, "\n")
+	}
+
+	fmt.Fprintf(&b, "Functions:\n")
+	for _, fn := range d.Funcs {
+		fmt.Fprintf(&b, "\t")
+		if err := printCfg.Fprint(&b, fs, fn.Decl); err != nil {
+			return nil, err
+		}
+		fmt.Fprintf(&b, "\n")
+	}
+
+	return b.Bytes(), nil
 }
