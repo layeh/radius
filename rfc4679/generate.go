c1a9c5fe82858f23fb6dc78740307c4f2f0941b8
Dominik Weidenfeld <dominik@weidenfeld.info>
2020-03-11 08:54:11 +0100

    add rfc4679
    Closes #65

---
+++ rfc4679/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run ../cmd/radius-dict-gen/main.go -package rfc4679 -ignore ADSL-Forum-DHCP-Vendor-Specific -ignore ADSL-Forum-Device-Manufacturer-OUI -ignore ADSL-Forum-Device-Serial-Number -ignore ADSL-Forum-Device-Product-Class -ignore ADSL-Forum-Gateway-Manufacturer-OUI -output generated.go /usr/share/freeradius/dictionary.rfc4679
+
+package rfc4679
