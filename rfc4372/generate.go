c838977dac6a313e4de612abbd1a2ae0cbcb086e
Dominik Weidenfeld <dominik@weidenfeld.info>
2020-03-11 07:54:54 +0100

    add rfc4372
    Closes #64

---
+++ rfc4372/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run ../cmd/radius-dict-gen/main.go -package rfc4372 -output generated.go -ref Error-Cause:layeh.com/radius/rfc3576 /usr/share/freeradius/dictionary.rfc4372
+
+package rfc4372
