99b3878d251841cd24c051341f6b30eb3b8c9cdc
Tim Cooper <tim.cooper@layeh.com>
2020-07-22 07:25:39 -0500

    add rfc5607

---
+++ rfc5607/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run ../cmd/radius-dict-gen/main.go -package rfc5607 -output generated.go -ref Service-Type:layeh.com/radius/rfc2865 /usr/share/freeradius/dictionary.rfc5607
+
+package rfc5607
