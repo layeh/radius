006f7d3a741d770b70c8e09c2f4a589513a2b9bc
Dalya Dickstein <dalyadickstein@fb.com>
2020-10-07 17:30:40 -0400

    Adding 6 new vendors and generated files

---
+++ vendors/ciena/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run ../../cmd/radius-dict-gen/main.go -package ciena -output generated.go dictionary.ciena
+
+package ciena
---
+++ vendors/infinera/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run ../../cmd/radius-dict-gen/main.go -package infinera -output generated.go dictionary.infinera
+
+package infinera
